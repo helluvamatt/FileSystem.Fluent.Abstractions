ba8c26b94cd9fc480e04b2ae153f43936c668bd4
Daniel Ware <Ware.daniel.a@gmail.com>
2020-12-18 20:47:21 -0600

    initial implementation of net5 apis

---
+++ FileSystem.Fluent.Abstractions.Net5/Net5Directory.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using FileSystem.Fluent.Abstractions.Core;
+
+namespace FileSystem.Fluent.Abstractions.Net5
+{
+    public class Net5Directory : IDirectory
+    {
+        public Net5Directory(string path)
+        {
+            Path = path;
+        }
+
+        public string Path { get; }
+
+        public IDirectory TryCreate()
+        {
+            try
+            {
+                Directory.CreateDirectory(Path);
+                return this;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public IDirectory Delete()
+        {
+            Directory.Delete(Path, false);
+            return this;
+        }
+
+        public IDirectory DeleteRecursive()
+        {
+            Directory.Delete(Path, true);
+            return this;
+        }
+
+        public IDirectory ForEachSubDirectory(Action<IDirectory> action)
+        {
+            string[] directoryNames = Directory.GetDirectories(Path);
+            foreach (string directoryName in directoryNames)
+            {
+                action?.Invoke(new Net5Directory(directoryName));
+            }
+            return this;
+        }
+
+        public IDirectory ForEachFile(Action<IFile> action)
+        {
+            string[] fileNames = Directory.GetFiles(Path);
+            foreach (string fileName in fileNames)
+            {
+                action?.Invoke(new Net5File(fileName));
+            }
+            return this;
+        }
+
+        public IDirectory WhenExists(Action<IDirectory> action)
+        {
+            if(Directory.Exists(Path)) action?.Invoke(this);
+            return this;
+        }
+
+        public IDirectory WhenNotExists(Action<IDirectory> action)
+        {
+            if(!Directory.Exists(Path)) action?.Invoke(this);
+            return this;
+        }
+
+        public IDirectory ForParent(Action<IDirectory> action)
+        {
+            DirectoryInfo parent = Directory.GetParent(Path);
+            if (parent is not null)
+            {
+                action.Invoke(this);
+            }
+            return this;
+        }
+    }
+}
---
+++ FileSystem.Fluent.Abstractions.Net5/Net5File.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using FileSystem.Fluent.Abstractions.Core;
+
+namespace FileSystem.Fluent.Abstractions.Net5
+{
+    public class Net5File : IFile
+    {
+        public Net5File(string path)
+        {
+            Path = path ?? throw new ArgumentNullException(nameof(path));
+        }
+
+        public string Path { get; }
+
+        public IFile TryCreate()
+        {
+            try
+            {
+                FileStream fileStream = File.Create(Path);
+                fileStream.Close();
+                return this;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public IFile CopyTo(IFile destination, Action<IFile, IFile> postCopyAction = null)
+        {
+            File.Copy(Path, destination.Path);
+            postCopyAction?.Invoke(this, destination);
+            return this;
+        }
+
+        public IFile Delete()
+        {
+            File.Delete(Path);
+            return this;
+        }
+
+        public IFile OpenWrite(Action<Stream> action)
+        {
+            using Stream fileStream = new FileStream(Path, FileMode.OpenOrCreate, FileAccess.Write);
+            action?.Invoke(fileStream);
+            return this;
+        }
+
+        public IFile OpenRead(Action<Stream> action)
+        {
+            using Stream fileStream = new FileStream(Path, FileMode.OpenOrCreate, FileAccess.Read);
+            action?.Invoke(fileStream);
+            return this;
+        }
+
+        public IFile WriteContents(string contents, Encoding encoding = null)
+        {
+            byte[] bytes = Encoding.Default.GetBytes(contents);
+            using Stream fileStream = new FileStream(Path, FileMode.Truncate, FileAccess.Write);
+            fileStream.Write(bytes);
+            return this;
+        }
+
+        public IFile AppendContents(string contents, Encoding encoding = null)
+        {
+            byte[] bytes = Encoding.Default.GetBytes(contents);
+            OpenWrite(s =>
+            {
+                s.Position = s.Length;
+                s.Write(bytes);
+            });
+            return this;
+        }
+
+        public IFile ReadLines(Action<string> action, Encoding encoding = null, string newline = null)
+        {
+            OpenRead(s =>
+            {
+                using StreamReader sr = new(s);
+                string lineContent = sr.ReadLine();
+                action?.Invoke(lineContent);
+            });
+            return this;
+        }
+
+        public IFile ReadAllLines(Action<string> action, Encoding encoding = null)
+        {
+            OpenRead(s =>
+            {
+                using StreamReader sr = new(s);
+                string contents = sr.ReadToEnd();
+                action?.Invoke(contents);
+            });
+            return this;
+        }
+
+        public IFile WhenExists(Action<IFile> action)
+        {
+            if(File.Exists(Path)) action?.Invoke(this);
+            return this;
+        }
+
+        public IFile WhenNotExists(Action<IFile> action)
+        {
+            if(!File.Exists(Path)) action?.Invoke(this);
+            return this;
+        }
+
+        public IFile ForParent(Action<IDirectory> action)
+        {
+            string directoryName = System.IO.Path.GetDirectoryName(Path);
+            action?.Invoke(new Net5Directory(directoryName));
+            return this;
+        }
+    }
+}
