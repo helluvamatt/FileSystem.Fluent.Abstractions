1203e6ef363804071c70ee0d9f0f665c6da1728e
Michael Koss <michaelkoss@gmail.com>
2021-01-03 09:52:47 -0600

    Create "Local" implementation. Create ReadOnly decorators and test.

---
+++ FileSystem.Fluent.Abstractions.Core.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FileSystem.Fluent.Abstractions.Core.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
